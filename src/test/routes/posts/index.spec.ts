115be3c300cd3de3867030ac73eb732c2323e590
coolemur <coolemur@gmail.com>
2021-11-01 09:36:03 +0200

    tests

--- src/test/routes/posts/index.spec.ts
+++ src/test/routes/posts/index.spec.ts
@@ -5,7 +5,12 @@ import createServer from 'server';
 const app = createServer();
 
 describe('posts routes', function () {
-  it('/posts responds with 200', function () {
+  it('/posts responds with 200', async function () {
+    await request(app).post('/auth/login').send({
+      'email': 'new_user@email.com',
+      'password': 'new_password'
+    }).expect(200);
+
     return request(app).get('/posts').expect(403);
   });
 });
